84a6e37e8d6efc2ee9028370fed5c8b8cf3deb51
Baruch <dolce_dmn@hotmail.com>
2016-12-27 21:51:55 -0600

    Creado el proyecto y la app, ademas de añadirse el template de base html

---
+++ social_noise/artists/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+# Create your models here.
+
+class Artist(models.Model):
+	name = models.CharField(max_length=100)
+	email = models.EmailField()
+
+class Musician(models.Model):
+    instrument = models.CharField(max_length=60)
+    musical_gender = models.CharField(max_length=50, blank=True, null=True)
+
+
+	def __str__(self)
+		return self.name
+		
